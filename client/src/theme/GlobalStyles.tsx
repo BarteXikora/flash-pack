545cb51a8b64f8ec9792da00b645bdaf598d08bc
Bartłomiej Sikora <sbartek50@gmail.com>
2025-10-24 15:53:34 +0200

    add montserrat font

--- client/src/theme/GlobalStyles.tsx
+++ client/src/theme/GlobalStyles.tsx
@@ -4,13 +4,11 @@ const GlobalStyles = createGlobalStyle`
     html, body, #root {
         margin: 0;
         padding: 0;
+        font-family: "Montserrat", sans-serif;
+        font-weight: 400;
         font-size: 16px;
         background-color: ${p => p.theme.color.black};
         color: ${p => p.theme.color.white};
-    }
-
-    .test {
-        ${p => p.theme.text.title}
     }
 
     .accent-primary {
