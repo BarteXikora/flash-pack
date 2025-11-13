8edf1f98a17bbeceb53c400be9b06274985cd194
Bartłomiej Sikora <sbartek50@gmail.com>
2025-11-13 20:31:21 +0100

    create section title component

---
+++ client/src/components/layout/sectionTitle/SectionTitle.tsx
@@ -0,0 +1,7 @@
+import StyledSectionTitle from './SectionTitle.styles'
+
+const SectionTitle = ({ children }: { children: React.ReactNode }) => {
+    return <StyledSectionTitle>{children}</StyledSectionTitle>
+}
+
+export default SectionTitle
