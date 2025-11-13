0be29ce5b8987691543a5f606214efdc2562b6a6
Bartłomiej Sikora <sbartek50@gmail.com>
2025-11-13 20:35:28 +0100

    add home page to the router

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import MainView from '../components/layout/mainView/MainView'
+import HomePage from '../pages/homePage/HomePage'
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<MainView />}>
-                    <Route path='' element={'Home Page'} />
+                    <Route path='' element={<HomePage />} />
 
                     <Route path='*' element={'404'} />
                 </Route>
